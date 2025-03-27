a4e0ed89a5884cf1a7a1a71434accdb10861c34c
Matilde Morrone <contact@morrone.dev>
2025-03-27 13:01:55 +0100

    Implement extraction logic in xtask crate

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,4 +1,7 @@
+use anyhow::{Context, Result, bail};
 use clap::{Parser, Subcommand};
+use std::fs;
+use xshell::{Shell, cmd};
 
 #[derive(Parser)]
 struct Cli {
@@ -11,12 +14,51 @@ pub enum Commands {
     Extract,
 }
 
-fn main() {
+const VERSIONS: [&str; 8] = [
+    "SLUS-20685", // USA
+    "SCCS-40001", // China(?)
+    "SCES-50885", // Europe
+    "SCES-51102", // France
+    "SCES-51104", // Germany
+    "SCES-51103", // Italy
+    "SCES-51105", // Spain
+    "SCPS-15025", // Original Japanese release
+];
+
+fn main() -> Result<()> {
     let Cli { commands } = Cli::parse();
 
     match commands {
         Commands::Extract => {
-            println!("Extracting...")
+            // Check if the files are actually there
+            for version in VERSIONS {
+                // TODO: do some sort of checksum
+                if !fs::exists(format!("isos/{version}.iso"))
+                    .context("Failed to verify path existance")?
+                {
+                    bail!("Missing iso file for {version}")
+                }
+            }
+
+            let sh = Shell::new()?;
+
+            if fs::exists("extracted")? {
+                fs::remove_dir_all("extracted")?;
+            }
+
+            fs::create_dir_all("extracted")?;
+
+            for version in VERSIONS {
+                fs::create_dir_all(format!("extracted/{version}"))?;
+
+                cmd!(
+                    sh,
+                    "bsdtar -xpkvf isos/{version}.iso -C extracted/{version}"
+                )
+                .run()?;
+            }
         }
     }
+
+    Ok(())
 }
